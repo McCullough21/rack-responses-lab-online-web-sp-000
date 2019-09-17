babae5b0a629928af4c670e47c77b01d156d6941
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-17 13:40:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-
-    if Time.new.hour < 12
+    t = Time.now
+    if t.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
